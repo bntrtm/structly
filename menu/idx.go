8f61059e3f2cbbeda45e04d8e9080b7e2e47d014
bntrtm <git.bntrtm@tjbainter.com>
2026-04-05 00:24:47 -0500

    feat(idx): add blacklist tag
    Add a new tag for blacklisting struct fields at the type level. This tag
    is logically incompatible with the idx tag. Interopability with the idx
    tag has been implemented to ensure they are never used on the same struct
    field, and that fields blacklisted at the type level with struct tags are
    ignored where necessary.

--- menu/idx.go
+++ menu/idx.go
@@ -6,31 +6,51 @@ import (
 	"strconv"
 )
 
-// getStructIdxMap returns a map of `idx` tag values which
-// correspond to the indeces of struct fields within the struct
-// represented by the given reflect.Type, in the order they are
-// declared. The presence or non-presence of the tags is
-// validated when reading each field.
+// getOrderedFields accounts for the presence of `idx` and `bl`
+// tags to provide a map that defines the order by which each
+// struct fields ought be rendered in the terminal.
 //
-// If the first field is found to have an `idx` tag, all others
-// will be expected to have one as well. Likewise, if it does not
-// have the tag, all others will be expected to not have the tag.
+// If the `idx` tag is in use on the struct,it returns a map of `idx`
+// tag values corresponding to the indeces of struct fields within the
+// struct represented by the given reflect.Type, in the order they
+// are declared. The presence or non-presence of the tags is validated
+// when reading each field. Fields blacklisted at the type level with
+// the `bl` tag are expected not to have an idx tag.
 //
-// Where validation fails, a nil map and error are
-// returned.
-func getStructIdxMap(t reflect.Type) (map[int]int, error) {
-	wantIdx := false
+// If the first non-blacklisted field is found to have an `idx` tag,
+// all others will be expected to have one as well. Likewise, if it
+// does not have the tag, all others will be expected not to have the tag.
+// When no `idx` tags are used, the map keys and values will match.
+//
+// Where validation fails, a nil map and error are returned.
+func getOrderedFields(t reflect.Type) (map[int]int, error) {
+	wantIdx := struct {
+		val   bool
+		isSet bool
+	}{val: false, isSet: false}
+
 	idxTagVals := map[int]int{}
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
 
-		tagValue, ok := field.Tag.Lookup("idx")
-		if ok && i == 0 {
-			wantIdx = true
+		_, isBlacklisted := field.Tag.Lookup("bl")
+		tagValue, isIndexed := field.Tag.Lookup("idx")
+
+		if isBlacklisted {
+			if isIndexed {
+				return nil, fmt.Errorf("incompatible struct tags; unexpected `idx` tag found on `bl`-tagged field %s", field.Name)
+			}
+			continue
 		}
 
-		if wantIdx {
-			if !ok {
+		// NOTE: at this point, can't possibly be blacklisted
+		if !wantIdx.isSet {
+			wantIdx.val = (len(idxTagVals) == 0 && isIndexed)
+			wantIdx.isSet = true
+		}
+
+		if wantIdx.val {
+			if !isIndexed {
 				return nil, fmt.Errorf("no `idx` tag found on struct field %s", field.Name)
 			}
 			idx, err := strconv.Atoi(tagValue)
@@ -41,7 +61,7 @@ func getStructIdxMap(t reflect.Type) (map[int]int, error) {
 				return nil, fmt.Errorf("value %d for `idx` tag on field %s already assigned to another field", val, field.Name)
 			}
 			idxTagVals[idx] = i
-		} else if ok {
+		} else if isIndexed {
 			return nil, fmt.Errorf("unexpected `idx` tag found on field %s", field.Name)
 		}
 
